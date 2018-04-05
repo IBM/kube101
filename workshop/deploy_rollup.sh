f23beb0da2bf79f6b2707ab01b37a4d42c5026d2
Doug Davis <dug@us.ibm.com>
2018-04-05 10:44:08 -0700

    try to fix travis
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- workshop/deploy_rollup.sh
+++ workshop/deploy_rollup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 echo "Install IBM Cloud CLI"
-. ./install_bx.sh
+. workshop/install_bx.sh
 if [ $? -ne 0 ]; then
   echo "Failed to install IBM Cloud Container Service CLI prerequisites"
   exit 1
 fi
 
 echo "Login to IBM Cloud"
-. ./bx_login.sh
+. workshop/bx_login.sh
 if [ $? -ne 0 ]; then
   echo "Failed to authenticate to IBM Cloud Container Service"
   exit 1
 fi
 
 echo "Testing yml files for generalized namespace"
-. ./test_yml.sh
+. workshop/test_yml.sh
 if [ $? -ne 0 ]; then
   echo "Failed to find <namespace> in deployment YAML files"
   exit 1
 fi
 
 echo "Deploy pods for Stage 3..."
-. ./deploy.sh
+. workshop/deploy.sh
 if [ $? -ne 0 ]; then
   echo "Failed to Deploy pods for stage 3 to IBM Cloud Container Service"
   exit 1
