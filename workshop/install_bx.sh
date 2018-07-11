03610d89f63602e7759f0dc4664875e58964e260
art <arthur.berger@ibm.com>
2018-07-11 11:20:40 -0400

    Removing the alias part since alias comes with plugin

--- workshop/install_bx.sh
+++ workshop/install_bx.sh
@@ -4,14 +4,6 @@ echo "Download IBM Cloud CLI"
 wget --quiet --output-document=/tmp/Bluemix_CLI_amd64.tar.gz  http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/latest/Bluemix_CLI_amd64.tar.gz
 tar -xf /tmp/Bluemix_CLI_amd64.tar.gz --directory=/tmp
 
-# Create bx alias
-echo "#!/bin/sh" >/tmp/Bluemix_CLI/bin/bx
-echo "/tmp/Bluemix_CLI/bin/bluemix \"\$@\" " >>/tmp/Bluemix_CLI/bin/bx
-chmod +x /tmp/Bluemix_CLI/bin/*
-chmod +x /tmp/Bluemix_CLI/bin/cfcli/*
-
-export PATH="/tmp/Bluemix_CLI/bin:$PATH"
-
 # Install IBM Cloud CS plugin
 echo "Install the IBM Cloud Container Service plugin"
 bx plugin install container-service -r Bluemix
